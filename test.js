0ce9fcf6694d1a8e39d33eb77b94d08d481de10c
khayreddine <ltayefkhayreddine123@gmail.com>
2023-02-28 20:58:56 +0100

    first commit

---
+++ test.js
@@ -0,0 +1,39 @@
+console.log("HELLO WORLD");
+// const http = require("http");
+const fs = require("fs");
+
+// var fs = require('fs');
+
+// fs.appendFile("welcome.txt", "Hello Node", function (err) {
+//   if (err) throw err;
+//   console.log("Saved!");
+// });
+
+// const hostname = "127.0.0.1";
+// const port = 3000;
+
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader("Content-Type", "text/plain");
+//   res.end("<h1>Hello Node!!!!</h1>");
+// });
+
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
+
+// fs.readFile("text.txt", "utf8", (err, data) => {
+//   if (err) {
+//     console.error(err);
+//     return;
+//   }
+//   console.log(data);
+// });
+// fs.appendFile("password-generator", "", function (err) {
+//   if (err) throw err;
+//   console.log("Saved!");
+// });
+// fs.appendFile(" email-sender", "Hello Node", function (err) {
+//   if (err) throw err;
+//   console.log("Saved!");
+// });
